fe29f324b893c97e4b0b534b4c7b6dfae59aa4fd
pranto-islam <prantoislamnt51@gmail.com>
2024-11-21 00:09:40 +0600

    Call Buy Value

--- Chaptra - 04/Call by value.cpp
+++ Chaptra - 04/Call by value.cpp
@@ -1,21 +1,22 @@
 
-
+// Call by value in C++
 #include <iostream>
 using namespace std;
-void valeu (int a, int b){
-    a = 100;
-    b = 200;
-    cout << a << endl << b << endl;
+void valeu (int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
 };
 // Main function -----
 int main() {
     
     int x = 10;
     int y = 20;
-
+    // Print the value of x and y before swapping
+    cout << x << endl << y;
+    // Print the value of x and y after swapping
     valeu(x,y);
     cout << x << endl << y;
-
     return 0;
 }
 
