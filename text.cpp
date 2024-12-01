aadf617621d52bc603a587abed984e5ac76e767e
pranto-islam <prantoislamnt51@gmail.com>
2024-12-01 07:15:31 +0600

    Add Factorial

--- text.cpp
+++ text.cpp
@@ -1,2 +1,63 @@
 
 
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+void factorial(int items){
+    int a = 0;
+    int b = 1;
+
+    cout << a << " ";
+    cout << b << " ";
+    for (int i = 2; i < items; i++){
+        int c = a + b;
+        cout << c << " ";
+        a = b;
+        b = c;
+    };
+};
+int main() {
+    int items;
+    cout << "Enter the number of items: ";
+    cin >> items;
+    factorial(items);
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
