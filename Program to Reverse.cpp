14734317ab45323b190aaae0e6ae3ad56bb66b0d
pranto-islam <prantoislamnt51@gmail.com>
2024-12-02 02:13:35 +0600

    none

--- Program to Reverse.cpp
+++ Program to Reverse.cpp
@@ -13,13 +13,12 @@ int main() {
 
     while (n > 0) {
         a = n % 10; // a Stores the number of the last digit
-        sum = sum * 10 + a;
-        n = n / 10;
-    }
+        sum = sum * 10 + a; // sum is multiplied by 10 and added to the last digit
+        n = n / 10; // n is the number of digits
+    };
     cout << sum << endl; // Output the reversed number
-    
     return 0;
-}
+};
 
 
 
