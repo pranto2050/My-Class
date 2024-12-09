bcf65a206854aa3826d9f7bb730c87bcd7ce8439
pranto-islam <prantoislamnt51@gmail.com>
2024-12-09 20:46:51 +0600

    Done Making

--- Bank management system in c++/bank management system.cpp
+++ Bank management system in c++/bank management system.cpp
@@ -22,41 +22,36 @@ class Bank_Account {
 private:
     string account_holder_name;
     int account_balance;
-
 public:
     // Parameterized constructor
     Bank_Account(string name, int balance) {
         account_holder_name = name;
         account_balance = balance;
     };
-
     // Display account details----------
     void displayAccountDetails() {
         cout << "Account Holder Name is: " << account_holder_name << endl;
         cout << "Account Balance: " << account_balance << endl;
     };
-
     // Withdrow from account ------ --
     void withdraw(int amount) {
         if (amount > account_balance) {
-            cout << "Insufficient balance!" << endl;
+            cout << " Insufficient balance!" << endl;
         } else {
             account_balance = account_balance - amount;
             cout << "Withdrawal successful!" << endl << "Your new balance is: " << account_balance << endl << endl;
         };
     };
-
     // Add balance------ 
     void addBalance(int amount) {
         account_balance += amount;
-        cout << "Balance added successfully! New balance: " << account_balance << endl;
+        cout << "Balance added successfully!" << endl << "New balance: " << account_balance << endl << endl;
     };
 };
 
 int main() {
     // Creating an object of Bank_Account
     Bank_Account account("Md.Pranto Islam", 99999999);
-
     // Display account details
     account.displayAccountDetails();
     // Withdraw from account
