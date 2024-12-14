600e869068a59bd4fdaa5d4217a8a4e89e459c12
pranto-islam <prantoislamnt51@gmail.com>
2024-12-14 21:00:43 +0600

    Remove Unused  Space 011

--- Chaptra - 06/6.5 - Constructor with default argument.cpp
+++ Chaptra - 06/6.5 - Constructor with default argument.cpp
@@ -2,11 +2,8 @@
 
 
 // Constructor with default argument -- in c++
-
-
 #include <iostream>
 using namespace std;
-
 class Student {
 private:
     string name;
@@ -18,18 +15,15 @@ public:
         name = studentName; // Parameter name is assigned to the name variable
         age = studentAge; // Parameter age is assigned to the age variable
         cout << "Constructor called: " << name << ", Age: " << age << endl;
-    }
+    };
 };
-
 int main() {
     cout << "Name: MD.Pranto Ali" << endl << "ID: 23303101" << endl;
     // Using default values
     Student student1; // Default constructor called: Unknown, Age: 0
-
     // Providing only the name, age will use the default value
     Student student2("Md.Ikbal Hossain"); // Constructor called: Md.Ikbal Hossain, Age: NULL
-
     // Providing both name and age
     Student student3("MD.Irfan Islam", 18); // Constructor called: MD.Irfan Islam, Age: 18
     return 0;
-}
+};
