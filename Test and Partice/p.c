105062e9b56f38fb5f1d0d25525a03441b9e09dc
pranto-islam <prantoislamnt51@gmail.com>
2024-12-24 18:06:34 +0600

    Add initial implementation of a simple C program to read and print two integers

---
+++ Test and Partice/p.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int rahim, karim; // Declares two integer variables rahim and karim
+    scanf("%d %d", &rahim, &karim); // Reads two integers from the user
+    printf("%d %d", rahim, karim); // Prints the two integers
+    return 0; // Terminates the program
+};
