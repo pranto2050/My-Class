b919dce07b11dcfbe8cdb79af52b549624b18099
pranto-islam <prantoislamnt51@gmail.com>
2024-12-26 23:34:43 +0600

    Remove unused bank_two class and simplify main function to use bank class directly

--- Test and Partice/constractor/basic.cpp
+++ Test and Partice/constractor/basic.cpp
@@ -16,26 +16,8 @@ public:
     }
 };
 
-class bank_two : public bank {
-protected:
-    int balance;
-public:
-    bank_two(int a, string n, int b) : bank(a, n) {
-        balance = b;
-    }
-    void deposit(int d) {
-        balance += d;
-    }
-    void display() {
-        bank::display();
-        cout << "Balance: " << balance << endl;
-    }
-};
-
 int main() {
-    bank_two b(12345, "John Doe", 1000);
-    b.display();
-    b.deposit(500);
+    bank b(12345, "John Doe");
     b.display();
 
     return 0;
