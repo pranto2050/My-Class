71e649ab00af79affbf92ef57a730f3734d5841b
pranto-islam <prantoislamnt51@gmail.com>
2025-01-04 16:07:15 +0600

    Add implementation of a C++ program to sum two integers

---
+++ Problems And Solve/099.cpp
@@ -0,0 +1,14 @@
+
+
+
+// Sum of two numbers using function
+#include <iostream>
+using namespace std;
+int sum(int a, int b) {
+    return a + b;
+}
+int main() {
+    int x = 10, y = 20;
+    cout << "Sum of " << x << " and " << y << " is " << sum(x, y) << endl;
+    return 0;
+};
