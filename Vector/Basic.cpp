624b9a32f7f42175226c7a39bd76fd617dbbc9a6
pranto-islam <prantoislamnt51@gmail.com>
2025-01-11 15:33:28 +0600

    Add Basic vector sorting implementation and Deauthor functionality for WiFi management

---
+++ Vector/Basic.cpp
@@ -0,0 +1,33 @@
+
+
+
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+int main() {
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(6);
+    v.push_back(33);
+    v.push_back(8);
+    v.push_back(23);
+    v.push_back(454);
+
+    int siz = v.size();
+    cout << siz << endl;
+    sort (v.begin(), v.end());
+    for (int i = 0; i < siz; i++)
+    {
+        cout << v[i] << " ";
+    }
+    
+
+
+    return 0;
+}
